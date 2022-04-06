92337e87f2ae9bfeb8333100aac87b9e6f9fb94e
tarunmali <tarunmalibackup2@gmail.com>
2022-04-06 12:27:14 +0530

    Signup signin working, clean code of signin

--- backend/Api/User.js
+++ backend/Api/User.js
@@ -1,6 +1,4 @@
-const db2=require('../server.js');
 const express=require('express');
-const User=require('../DB/User');
 const route=express.Router();
 const Sequelize = require('sequelize');
 const { QueryTypes } = require('sequelize');
@@ -15,12 +13,12 @@ route.post('/',async(req,res)=>
     const {name, email, phone, work, password, cpassword}=req.body;
     if (name==="" || email===""  || work==="" || password==="" || cpassword==="" || phone==="") 
     {
-       return res.status(422).json({error:"Please fill all the fields"}); 
+       return res.status(422).json({error:"Please  fill all the fields"}); 
     }
-    const [result, metadata] = await sequelize.query(`SELECT * FROM user WHERE email='${email}'`);
+    const [result, metadata] = await sequelize.query(`SELECT * FROM user WHERE email='${email}'`, { type: QueryTypes.SELECT });
 
-    console.log(result);
-if(typeof yourVariable === 'object')
+    console.log();
+if(typeof result === 'object')
 {
     return res.status(422).json({error:"User already exists"});
 }
@@ -39,9 +37,9 @@ else
         }
         else
         {
+
             let sql=`INSERT INTO user (name,email,work,password,phone) VALUES ("${name}","${email}","${work}","${password}",${phone});`;
-            let user={name:name,email:email,work:work,password:password,phone:phone};
-            await sequelize.query(sql,user);
+            await sequelize.query(sql);
             res.status(200).json({ message: "user registered successfuly" });
         }
     }
--- backend/Api/signin.js
+++ backend/Api/signin.js
@@ -1,95 +1,44 @@
 const express=require('express');
 const jwt=require('jsonwebtoken');
-const User=require('../DB/User');
 const route=express.Router();
 const bcrypt=require('bcrypt');
 const db=require('../server.js');
-
-// ReferenceError: require is not defined in ES module scope, you can use import instead
-
-
-// import express from 'express';
-// import jwt from 'jsonwebtoken';
-// import User from '../DB/User';
-// import route from 'express.Router';
-// import bcrypt from 'bcrypt';
-
-
-
+const Sequelize = require('sequelize');
+const { QueryTypes } = require('sequelize');
+const sequelize = new Sequelize("codecountry", "admin", "Welcome2011", {
+    host: "aws-simplified.ctvxs2sfgoaz.us-west-1.rds.amazonaws.com",
+    dialect: "mysql",
+  });
 
     //login route
      route.post('/',async(req,res)=>{
-
-        // res.send("Tarun Mali signin")
-
-         let token;
+        let token;
         const {email, password}=req.body;
-        if (email==="" || password==="") {
+        if (email==="" || password==="") 
+        {
            return res.status(422).json({error:"Please fill all the fields"}); 
-       }
+        }
 
-    //    const userLogin= await User.findOne({email:email});
-        
-        db.query(`SELECT * FROM user WHERE email='${email}' `,(err,result)=>{
-            if(err) throw err;
-            if(result.length>0)
+       const [result, metadata] = await sequelize.query(`SELECT * FROM user WHERE email="${email}"`,{ type: QueryTypes.SELECT });
+    
+        if(typeof result === 'object')
+        {
+            if(password==result.password)
             {
-                console.log(result);
-                const user=result[0];
-                console.log(user);
+                res.status(200).json({message:"User logged in successfully"});
 
-                if(password==user.password)
-                {
-                    res.status(200).json({message:"User logged in successfully"});
-
-                }
-
-                // if(bcrypt.compareSync(password, user.password)){
-                //     token=jwt.sign({_id:user._id}, 'secretkey');
-                //     console.log(token);
-                //     res.header('auth-token', token).send(token);
-                // }
-                // else{
-                //     return res.status(422).json({error:"Password does not match"});
-                // }
+            }
             else
             {
                 return res.status(422).json({error:"Incorrect Password"});
             }
-
-            }
-            else{
-                return res.status(422).json({error:"User does not exist"});
-            }
-
-        });
+        }
+        else
+        {
+            return res.status(422).json({error:"User does not exist"});
+        }
 
 
-
-        // if(!userLogin){
-        //     return res.status(422).json({error:"User does not exist"});
-        // }
-        // else{
-        //     token= await userLogin.generateAuthToken();
-
-        //     res.cookie('taruncookie',token,{
-        //         expires:new Date(Date.now()+25892000000),
-        //         httpOnly:true
-        //     });
-        //     //30 days in milliseconds
-        //     //so that it works also on http, else it wil work only on https
-
-        //     const isMatch=await bcrypt.compare(password,userLogin.password);
-        //     if(!isMatch){
-        //         return res.status(422).json({error:"Password is incorrect"});
-        //     }
-        //     else{
-        //         res.json({message:"User logged in successfully"});
-        //     }
-
-
-     
-     });
-
+      });
 
 module.exports=route;
