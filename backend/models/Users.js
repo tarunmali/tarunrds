851dfb2a6a528052653f93019e7e6f09210f0871
tarunmali <tarunmalibackup2@gmail.com>
2022-04-23 19:37:42 +0530

    fully functioal

---
+++ backend/models/Users.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+    const Users = sequelize.define("Users", {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      work: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+  
+    Users.associate = (models) => {
+      Users.hasMany(models.Posts, {
+        onDelete: "cascade",
+      });
+    };
+    return Users;
+  };
