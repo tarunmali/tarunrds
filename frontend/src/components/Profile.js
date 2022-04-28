2e2c4e38e2538a75e07a325bcbe61627d4cd3a58
tarunmali <tarunmalibackup2@gmail.com>
2022-04-29 01:37:02 +0530

    working suceesfully

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -25,21 +25,21 @@ function Profile() {
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_DATA}/profile/1`).then((response) => {
-      setPostObject(response.data);
+      setPostObject(response.data[0]);
     });
   }, []);
 
-  console.log("Santosh");
-  console.log(postObject);
+ 
 
   return (
     
     <div className="profilePageContainer">
       <div className="basicInfo">
-        <h1> Name: {postObject[0].name} </h1>
-        <h1> Email: {postObject[0].email} </h1>
-        <h1> Phone: {postObject[0].phone} </h1>
-        <h1> Work: {postObject[0].work} </h1>
+        <h1> Name: {postObject.name} </h1>
+        <h1> Email: {postObject.email} </h1>
+        <h1> Phone: {postObject.phone} </h1>
+        <h1> Work: {postObject.work} </h1>
+
         
       </div>
 
